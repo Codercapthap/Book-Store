2ccb76c7467f31cc095341b0837bee85e161293f
Coder đau bao tử <kidkaito1412.1@gmail.com>
2023-11-28 20:07:46 +0700

    update cors setting

--- Book-Store-Api/src/main/java/bookstore/security/SecurityConfig.java
+++ Book-Store-Api/src/main/java/bookstore/security/SecurityConfig.java
@@ -123,7 +123,7 @@ public class SecurityConfig {
 			public void addCorsMappings(
 					CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins(
-						"http://127.0.0.1:5173");
+						"http://127.0.0.1:5173", "http://localhost:5173");
 			}
 		};
 	}
